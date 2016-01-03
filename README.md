4fcfde2407269e3304ef054a820abc963851dd43
inkenbrandt <paulinkenbrandt@utah.gov>
2016-01-02 23:48:06 -0700

    Setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # WellApplication
 Tools used to manipulate hydrograph data; Solinst xles to pandas dataframe; hydrograph analysis
+
+
+================
+Well Application
+================
+
+Uses
+====
+
+This package:
+*allows a user to upload data from an .xle file common with some water well transducers.
+
+*matches well and barometric data to same sample intervals
+
+*adjust with manual measurements
+
+*removes skips and jumps from data
--- setup.py
+++ setup.py
@@ -1,20 +1,28 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
-setup(
-    name='WellApplication',
-    version='0.0.1',
-    author='Paul Inkenbrandt',
-    author_email='paulinkenbrandt@utah.gov',
-    packages=['wellapplication'],
-    scripts=[],
-    url='http://pypi.python.org/pypi/WellApplication/',
-    license='LICENSE.txt',
-    description='Interface with Solinst transducers; analyze hydrographs; plot hydrographs',
-    long_description=open('README.txt').read(),
-    install_requires=[
-        "Pandas >= 0.16.1",
-        "Numpy >= 1.9.0",
-	"Matplotlib >= 1.4.3",
-	"xmltodict >= 0.9.2"
-    ],
-)
+setup(name='WellApplication', 
+      version='0.0.6', 
+      author='Paul Inkenbrandt',
+      author_email='paulinkenbrandt@utah.gov',
+      packages=find_packages(exclude=['contrib', 
+'docs', 'tests*']),
+      url='http://pypi.python.org/pypi/WellApplication/',
+      license='LICENSE.txt',
+      description='Interface with xle files; analyze hydrographs; plot hydrographs',
+      long_description=open('README.txt').read(),
+      install_requires=["Pandas >= 0.16.1", 
+                        "Numpy >= 1.9.0", 
+                        "Matplotlib >= 1.4.3", 
+                        "xmltodict >= 0.9.2"],
+      classifiers = [
+                    'Development Status :: 1 - Planning',
+                    'Programming Language :: Python :: 2.7',
+                    'Intended Audience :: Science/Research',
+                    'Topic :: Scientific/Engineering :: GIS',
+                    'Natural Language :: English',
+                    'License :: OSI Approved :: MIT License',
+                    'Programming Language :: Python :: 2.7'
+                    ],
+    keywords='hydrogeology hydrograph barocorrection',
+    )
+    
