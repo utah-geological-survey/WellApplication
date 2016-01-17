0fb834e452dff170dc5942ca776a043c2bf02040
inkenbrandt <paulinkenbrandt@utah.gov>
2016-01-17 14:26:08 -0700

    Plot

--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
 from setuptools import setup, find_packages
 
 setup(name='WellApplication', 
-      version='0.1.1', 
+      version='0.1.3', 
       author='Paul Inkenbrandt',
       author_email='paulinkenbrandt@utah.gov',
       packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
       url='https://github.com/inkenbrandt/WellApplication',
       license='LICENSE.txt',
-      description='Interface with xle files; analyze hydrographs; plot hydrographs',
+      description='Interface with xle files; analyze hydrographs; plot hydrographs; download USGS data',
       install_requires=["Pandas >= 0.16.1", 
                         "Numpy >= 1.9.0", 
                         "Matplotlib >= 1.4.3", 
                         "xmltodict >= 0.9.2",
-                        "scipy >= 0.13.3"],
+                        "scipy >= 0.13.3",
+                        "pyproj >= 1.9.4"],
       classifiers = [
                     'Development Status :: 1 - Planning',
                     'Programming Language :: Python :: 2.7',
--- wellapplication/usgsGis.py
+++ wellapplication/usgsGis.py
@@ -9,6 +9,9 @@ import xmltodict
 import pandas as pd
 from datetime import datetime
 from httplib import BadStatusLine
+import numpy as np
+import matplotlib.pyplot as plt
+import avgMeths
 
 class usgs:
     @staticmethod 
@@ -103,7 +106,6 @@ class usgs:
         html = "http://waterservices.usgs.gov/nwis/site/?format=rdb&sites=" + siteno + "&siteOutput=expanded"
         siteinfo = self.getInfo(html)
         return siteinfo
-    
 
     def getStationInfoFromHUC(self, HUC):
         stationhtml = "http://waterservices.usgs.gov/nwis/site/?format=rdb,1.0&huc=" + str(HUC) + "&siteType=GW&hasDataTypeCd=gw"
@@ -132,6 +134,61 @@ class usgs:
         html = "http://waterservices.usgs.gov/nwis/gwlevels/?format=rdb&sites="+str(siteno)+"&startDT=1800-01-01&endDT="+str(datetime.today().year)+"-"+str(datetime.today().month).zfill(2)+"-"+str(datetime.today().day).zfill(2)
         wls = self.getInfo(html)
         return wls
-
+    
+    def hucPlot(self, HUC):
+        #stations = USGS.getStationsfromHUC(str(HUC))
+        siteinfo = self.getStationInfoFromHUC(str(HUC))
+        data = self.getWLfromHUC(HUC)
+        data.drop([u'agency_cd', u'site_tp_cd'], inplace=True, axis=1)
+        stationWL = pd.merge(data, siteinfo, on='site_no', how='left')
+        stationWL['date'], stationWL['Year'], stationWL['Month'] = zip(*stationWL['lev_dt'].apply(lambda x: avgMeths.getyrmnth(x),1))
+        stationWL.reset_index(inplace=True)
+        stationWL.set_index('date',inplace=True)
+        # get averages by year, month, and site number
+        grpstat = stationWL.groupby('site_no')['lev_va'].agg([np.std,np.mean,np.median, np.min,np.max,np.size]).reset_index()
+        USGS_Site_Inf = stationWL.groupby('site_no')['lev_dt'].agg([np.min,np.max,np.size]).reset_index()
+        USGS_Site_Info = USGS_Site_Inf[USGS_Site_Inf['size']>50]
+        wlLong = stationWL[stationWL['site_no'].isin(list(USGS_Site_Info['site_no'].values))]
+        wlLongStats = pd.merge(wlLong,grpstat, on='site_no', how='left')
+        wlLongStats['stdWL'] = wlLongStats[['lev_va','mean','std']].apply(lambda x: avgMeths.stndrd(x),1 )
+        wlLongStats['YRMO'] = wlLongStats[['Year','Month']].apply(lambda x: avgMeths.yrmo(x),1)
+        wlLongStats['date'] = wlLongStats[['Year','Month']].apply(lambda x: avgMeths.adddate(x),1)
+        wlLongStats.groupby(['Month'])['stdWL'].mean().to_frame().plot()
+        wlLongStats['levDiff'] = wlLongStats['lev_va'].diff()
+            
+        wlLongStatsGroups = wlLongStats.groupby(['date'])['stdWL'].agg({'mean':np.mean,'median':np.median,
+                                                                        'standard':np.std, 
+                                                                        'cnt':(lambda x: np.count_nonzero(~np.isnan(x))), 
+                                                                        'err':(lambda x: 1.96*np.std(x)/np.count_nonzero(~np.isnan(x)))})
+        wlLongStatsGroups2 = wlLongStats.groupby(['date'])['levDiff'].agg({'mean':np.mean,'median':np.median, 'standard':np.std, 'cnt':(lambda x: np.count_nonzero(~np.isnan(x))), 'err':(lambda x: 1.96*np.std(x)/np.count_nonzero(~np.isnan(x)))})
+    
+        wlLongStatsGroups['meanpluserr'] = wlLongStatsGroups['mean'] + wlLongStatsGroups['err']
+        wlLongStatsGroups['meanminuserr'] = wlLongStatsGroups['mean'] - wlLongStatsGroups['err']
+    
+        wlLongStatsGroups2['meanpluserr'] = wlLongStatsGroups2['mean'] + wlLongStatsGroups2['err']
+        wlLongStatsGroups2['meanminuserr'] = wlLongStatsGroups2['mean'] - wlLongStatsGroups2['err']
+        
+        wlLongSt = wlLongStatsGroups[wlLongStatsGroups['cnt']>2]
+        wlLongSt2 = wlLongStatsGroups2[wlLongStatsGroups2['cnt']>2]
+        
+        plt.figure()
+        x = wlLongSt.index
+        y = wlLongSt['mean']
+        plt.plot(x,y,label='Average Groundwater Level Variation')
+        plt.fill_between(wlLongSt.index, wlLongSt['meanpluserr'], wlLongSt['meanminuserr'], 
+                         facecolor='blue', alpha=0.4, linewidth=0.5, label= "Std Error")
+        plt.grid(which='both')
+        plt.ylabel('Depth to Water z-score')
+        plt.xticks(rotation=45)
+        
+        plt.figure()
+        x = wlLongSt2.index
+        y = wlLongSt2['mean']
+        plt.plot(x,y,label='Average Groundwater Level Changes')
+        plt.fill_between(wlLongSt.index, wlLongSt2['meanpluserr'], wlLongSt2['meanminuserr'], 
+                         facecolor='blue', alpha=0.4, linewidth=0.5, label= "Std Error")
+        plt.grid(which='both')
+        plt.ylabel('Change in Average Depth to Water (ft)')
+        plt.xticks(rotation=45)
 
     
