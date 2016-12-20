4aa349715c3cca9ef7ed8463a484dc130d367c8e
Paul <paulinkenbrandt@utah.gov>
2016-12-20 12:49:59 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -5,19 +5,12 @@ Created on Sat Jan 23 13:03:00 2016
 @author: p
 """
 
-import unittest
 import wellapplication as wa
 
 
 
-class TestMethods(unittest.TestCase):
-    def setUp(self):
-        pass
-    
-    def test_getelev(self):
-        x = [-111.21,41.4]
-        g = wa.getelev(x)
-        self.assertTrue(g > 100.0)
+def test_getelev(self):
+    x = [-111.21,41.4]
+    g = wa.getelev(x)
+    assert g > 100.0
 
-if __name__ == '__main__':
-    unittest.main()
