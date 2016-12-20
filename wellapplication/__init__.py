f0feac66ad308ae2d0602751fcaf5552f191dd32
Paul <paulinkenbrandt@utah.gov>
2016-12-20 06:15:57 -0700

    Update __init__.py

--- wellapplication/__init__.py
+++ wellapplication/__init__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.2.23'
+__version__ = '0.2.24'
 __author__ = 'Paul Inkenbrandt'
 __name__ = 'wellapplication'
 
 from transport import *
-from usgs import usgs
+from usgs import *
 from chem import WQP
 from graphs import piper, fdc, gantt
 import MannKendall
