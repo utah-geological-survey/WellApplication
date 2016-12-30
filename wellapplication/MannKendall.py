8573aaff796110baba17495f2463e7a73b9e07c7
Paul <paulinkenbrandt@utah.gov>
2016-12-29 17:03:55 -0700

    Update MannKendall.py

--- wellapplication/MannKendall.py
+++ wellapplication/MannKendall.py
@@ -34,7 +34,7 @@ def mk_test(x, alpha = 0.05):
     Credit: http://pydoc.net/Python/ambhas/0.4.0/ambhas.stats/
     """
     n = len(x)
-    
+    ta = n*(n-1)/2
     # calculate S 
     s = 0
     for k in xrange(n-1):
@@ -74,7 +74,7 @@ def mk_test(x, alpha = 0.05):
     else:
         trend = 'no trend'
         
-    return pd.Series({'trend':trend, 'varS':round(var_s,3), 'p':round(p,3), 'z':round(z,3), 's':round(s,3)})
+    return pd.Series({'trend':trend, 'varS':round(var_s,3), 'p':round(p,3), 'z':round(z,3), 's':round(s,3), 'n':n, 'ta':ta})
     
 def mk_ts(df, const, group1, orderby = 'year', alpha = 0.05):
     '''
@@ -84,77 +84,6 @@ def mk_ts(df, const, group1, orderby = 'year', alpha = 0.05):
     orderby = variable to order by (typically a date)
     '''
     
-    def mk_test(x, alpha):
-        """
-        this perform the MK (Mann-Kendall) test to check if there is any trend present in 
-        data or not
-
-        Args:
-            x:   
-                a vector of data
-            alpha: 
-                significance level
-
-        Returns:
-            trend: 
-                tells the trend (increasing, decreasing or no trend)
-            h: 
-                True (if trend is present) or False (if trend is absence)
-            p: 
-                p value of the sifnificance test
-            z: 
-                normalized test statistics 
-
-        Examples::
-          >>> x = np.random.rand(100)
-          >>> trend,h,p,z = mk_test(x,0.05) 
-
-        Credit: http://pydoc.net/Python/ambhas/0.4.0/ambhas.stats/
-        """
-        n = len(x)
-        ta = n*(n-1)/2
-        # calculate S 
-        s = 0
-        for k in xrange(n-1): # this does not work with nan values
-            for j in xrange(k+1,n):
-                s += np.sign(x[j] - x[k])
-
-        # calculate the unique data
-        unique_x = np.unique(x)
-        g = len(unique_x)
-
-        # calculate the var(s)
-        if n == g: # there is no tie
-            var_s = (n*(n-1)*(2*n+5))/18
-        else: # there are some ties in data
-            tp = np.zeros(unique_x.shape)
-            for i in xrange(len(unique_x)):
-                tp[i] = sum(unique_x[i] == x)
-            var_s = (n*(n-1)*(2*n+5) - np.sum(tp*(tp-1)*(2*tp+5)))/18
-        
-        # calculate z value
-        if s > 0.0:
-            z = (s - 1)/np.sqrt(var_s)
-        elif s == 0.0:
-            z = 0
-        elif s < 0.0:
-            z = (s + 1)/np.sqrt(var_s)
-        else:
-            print('problem with s')
-
-        # calculate the p_value
-        p = 2*(1-norm.cdf(abs(z))) # two tail test
-        h = abs(z) > norm.ppf(1-alpha/2) 
-
-        if (z<0) and h:
-            trend = 'decreasing'
-        elif (z>0) and h:
-            trend = 'increasing'
-        else:
-            trend = 'no trend'
-
-        return pd.Series({'trend':trend, 'varS':round(var_s,3), 'p':round(p,3), 'z':round(z,3), 's':round(s,3), 'n':n, 'ta':ta})
-
     def zcalc(Sp, Varp):
         if Sp > 0:
             return (Sp - 1)/Varp**0.5
