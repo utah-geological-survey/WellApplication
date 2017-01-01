289c4b815f1ff690e6716914f097cacb56052295
Paul <paulinkenbrandt@utah.gov>
2017-01-01 08:58:59 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -9,45 +9,37 @@ import wellapplication as wa
 import matplotlib
 import pandas as pd
 
-def test_getelev():
-    x = [-111.21,41.4]
-    g = wa.getelev(x)
-    assert g > 100.0
-
-def test_gethuc():
-    x = [-111.21,41.4]
-    g = wa.get_huc(x)
-    assert len(g[0])>0
-
-def test_USGSID():
-    x = [-111.21,41.4]
-    g = wa.USGSID([-111.21,41.4])
-    assert g == '412400111123601'
-
-def test_get_nwis():
-    val_list = '01585200'
-    g = wa.get_nwis(val_list, 'dv_site', '2012-06-01', '2012-07-01')
-    assert len(g) == 2
-
-def test_get_nwis_huc():
-    g = wa.get_nwis('16010204', 'gw_huc')
-    assert len(g) == 2
+class test_usgs:
     
-def test_get_station_info():
-    assert len(wa.get_station_info(['01585200','10136500'])) == 2
-    
-def test_cleanGWL():
-    stat, levs = wa.get_nwis('16010204', 'gw_huc')
-    levs2 = wa.cleanGWL(levs)
-    assert type(levs2.qualifiers[0]) == str
-    
-def test_avg_wl():
-    g = wa.avg_wl('16030006',numObs= 50, grptype = 'monthly', avgtype = 'avgDiffWL')
-    assert len(g)==12
+    def __init__(self):
+        x = [-111.21,41.4]
+        val_list = '01585200'
+        val_huc = '16010204'
+        stat, levs = wa.get_nwis(val_huc, 'gw_huc')
+        g = wa.get_nwis(val_list, 'dv_site', '2012-06-01', '2012-07-01')
+        f = wa.avg_wl('16030006',numObs= 50, grptype = 'monthly', avgtype = 'avgDiffWL')
+        
+    def test_getelev(self):
+        g = wa.getelev(self, x)
+        assert g > 100.0
 
-def test_plt_avg_wl():
-    g = wa.avg_wl('16030006',numObs= 50, grptype = 'monthly', avgtype = 'avgDiffWL')
-    assert type(wa.plt_avg_wl(g)) == matplotlib.figure.Figure
+    def test_gethuc(self, x):
+        huc_data = wa.get_huc(x)
+        assert len(g[0])>0
+
+    def test_USGSID(self, x):
+        usgs_id = wa.USGSID([-111.21,41.4])
+        assert g == '412400111123601'
+
+    def test_get_station_info(self):
+        assert len(wa.get_station_info(['01585200','10136500'])) == 2
+    
+    def test_cleanGWL(self, levs):
+        levs2 = wa.cleanGWL(levs)
+        assert type(levs2.qualifiers[0]) == str
+
+    def test_plt_avg_wl(self, f):
+        assert type(wa.plt_avg_wl(f)) == matplotlib.figure.Figure
 
 def test_mktest():
     x = range(0,100)
@@ -92,18 +84,6 @@ def test_mkts():
     usgsP.dropna(inplace=True)
     g = wa.MannKendall.mk_ts(usgsP, 'PO4', 'month', 'year',0.05)
     assert g.S == -87
-
-def test_piper():
-    pipr = wa.piper()
-    Chem =  {'Type':[1,2,2,3], 'Cl':[1.72,0.90,4.09,1.52], 'HCO3':[4.02,1.28,4.29,3.04], 
-             'SO4':[0.58,0.54,0.38,0.46], 'NaK':[1.40,0.90,3.38,2.86], 'Ca':[4.53,None,4.74,1.90], 
-             'Mg':[0.79,0.74,0.72,0.66], 'EC':[672.0,308.0,884.0,542.0], 'NO3':[0.4,0.36,0.08,0.40], 
-             'Sicc':[0.21,0.56,None,-0.41]}  
-    chem = pd.DataFrame(Chem)
-    chem1 = pipr.fillMissing(chem)
-    chem2 = pipr.convertIons(chem1)
-    chem3 = pipr.ionPercentage(chem2)
-    assert "CO3EC" in list(chem3.columns)
     
 def test_pipe():
     pipr = wa.piper()
@@ -119,13 +99,6 @@ def test_fdc():
     sites,levs = wa.get_nwis('01585200')
     assert len(wa.fdc(levs,'value')[0]) > 100
 
-def test_gannt():
-    sites,levs = wa.get_nwis('01585200')
-    levs.drop(['qualifiers','site_no'],axis=1,inplace=True)
-    levs['value'] = pd.to_numeric(levs['value'])
-    gnt = wa.gantt(levs,stations=['value'])
-    assert gnt.sitestats['StationId'].values[0] == 'value'
-
 def test_ganntplotter():
     sites,levs = wa.get_nwis('01585200')
     levs.drop(['qualifiers','site_no'],axis=1,inplace=True)
@@ -140,5 +113,4 @@ def test_recess():
 def test_reccur():
     cession = wa.graphs.recess()
     df = wa.get_nwis('01585200',selectType='dv_site', start_date='1968-01-01', end_date='1968-06-01')[1]
-    df['value'].plot()
     type(cession.recession(df,'value',[1968,1,15],[1968,1,20])[1]) == pd.indexes.numeric.Float64Index
