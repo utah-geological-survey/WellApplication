0415c250ade7f75e129cd19618fbacce90882976
Paul <paulinkenbrandt@utah.gov>
2017-01-06 00:12:10 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -95,39 +95,29 @@ def testmetadata():
 
 def testtimeseries():
     timeseries = m.timeseries(stid='kfnl', start='201504261800', end='201504262300')
-    ok_(timeseries)
-
 
 def testclimatology():
     climatology = m.climatology(stid='kden', startclim='04260000', endclim='04270000', units='precip|in')
-    ok_(climatology)
-
 
 def testprecip():
     precip = m.precip(stid=['kfnl', 'ksdf'], start='201504261800', end='201504271200', units='precip|in')
-    ok_(precip)
+
 
 def testclimatestats():
     climate_stats = m.climate_stats(stid='mtmet', startclim='03240000', endclim='03280000', type='all')
-    ok_(climate_stats)
 
 def testtimestats():
     stats = m.time_stats(stid='mtmet', start='201403240000', end='201403280000', type='all')
-    ok_(stats)
 
 def testlatency():
     latency = m.latency(stid='mtmet', start='201403240000', end='201403280000')
-    ok_(latency)
 
 def testnettypes():
     nettypes = m.networktypes()
-    ok_(nettypes)
 
 def testnetworks():
     networks = m.networks()
-    ok_(networks)
 
 def testattime():
     attime = m.attime(stid='kfnl', attime='201504261800', within='30')
-    ok_(attime)
 
