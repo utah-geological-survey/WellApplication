f88d06aaf1bda4af7e8d25c7669a44b657136dc9
Paul <paulinkenbrandt@utah.gov>
2017-01-07 23:28:28 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -87,3 +87,9 @@ def testvars():
 def testmetadata():
     stations = m.metadata(radius=['wbb', 5])
     
+def test_WQP():
+    wqq = WQP('16010204','huc')
+    wqq.massage_results()
+    pivchem = wqq.piv_chem()
+    assert 'ParAbb' isin pivchem.columns
+    
