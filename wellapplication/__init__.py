17afbb4ae7d9d623980299b770507e79e3738ab4
Paul <paulinkenbrandt@utah.gov>
2017-01-07 23:19:45 -0700

    usgs

--- wellapplication/__init__.py
+++ wellapplication/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
-__version__ = '0.4.2'
+__version__ = '0.4.3'
 __author__ = 'Paul Inkenbrandt'
 __name__ = 'wellapplication'
 
 from transport import *
 from usgs import *
-from chem import WQP
+from chem import *
 from mesopy import *
 from graphs import piper, fdc, gantt
 import MannKendall
