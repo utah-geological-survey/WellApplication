1dec50e6ab4d091ba7d255ed978e74f2840a72c1
Paul <paulinkenbrandt@utah.gov>
2017-01-09 21:10:29 -0700

    Update test.py

--- test/test.py
+++ test/test.py
@@ -133,3 +133,9 @@ def test_fix_drift():
     manual_35.set_index('dt',inplace=True)
     fd = wa.fix_drift(xle_df, manual_35, meas='Level', manmeas='MeasuredDTW', outcolname='DriftCorrection')
     assert 'DriftCorrection' in list(fd[0].columns)
+    
+def test_getwellid():
+    inputfile = "docs/ag13c 2016-08-02.xle"
+    wellinfo = pd.read_csv("docs/wellinfo4.csv")
+    wid = wa.getwellid(inputfile, wellinfo)
+    assert wid[1] == 35
