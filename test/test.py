1bd1d2e82be46e9511afb5502d9c0ef7291d1b12
inkenbrandt <paulinkenbrandt@utah.gov>
2017-01-11 08:24:20 -0700

    recess cleanup

--- test/test.py
+++ test/test.py
@@ -168,15 +168,10 @@ def test_jumpfix():
     jf = wa.jumpfix(df, 'Level', threashold=0.005)
     assert jf['newVal'][-1] > 10
 
-def test_gantt_plt():
-    logan = wa.nwis('dv', '10109000', 'sites')
-    gn = wa.gantt(logan.data, stations=['value'])
-    assert type(gn.ganttPlotter()) == matplotlib.figure.Figure
-
 def test_gantt():
-    logan = wa.nwis('dv', '10109000', 'sites')
-    gn = wa.gantt(logan.data, stations=['value'])
-    assert len(gn.sitestats) == 1
+    ashley = wa.nwis('dv', '09265500', 'sites')
+    gn = wa.gantt(ashley.data, stations=['value'])
+    assert type(gn.gantt()[2]) == matplotlib.figure.Figure
 
 def test_scatterColor():
     x = np.arange(1, 100, 1)
