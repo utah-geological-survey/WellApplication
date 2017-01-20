c135a53f328a3bde7d1ea5540cd7d0c70c9a2ee3
Paul <paulinkenbrandt@utah.gov>
2017-01-19 22:56:38 -0700

    Added test and moved test files

--- wellapplication/graphs.py
+++ wellapplication/graphs.py
@@ -688,3 +688,63 @@ def scatterColor(x0, y, w):
 
     return slope, const
 
+
+
+
+def peakdet(v, delta, x=None):
+    """
+    :param v: data vector
+    :param delta:
+    :param x:
+    :returns: two arrays
+
+    .. note:: https://gist.github.com/endolith/250860
+        Converted from MATLAB script at http://billauer.co.il/peakdet.html
+    """
+    import sys
+
+    maxtab = []
+    mintab = []
+
+    if x is None:
+        x = np.arange(len(v))
+
+    v = np.asarray(v)
+
+    if len(v) != len(x):
+        sys.exit('Input vectors v and x must have same length')
+
+    if not np.isscalar(delta):
+        sys.exit('Input argument delta must be a scalar')
+
+    if delta <= 0:
+        sys.exit('Input argument delta must be positive')
+
+    mn, mx = np.Inf, -np.Inf
+    mnpos, mxpos = np.NaN, np.NaN
+
+    lookformax = True
+
+    for i in np.arange(len(v)):
+        this = v[i]
+        if this > mx:
+            mx = this
+            mxpos = x[i]
+        if this < mn:
+            mn = this
+            mnpos = x[i]
+
+        if lookformax:
+            if this < mx - delta:
+                maxtab.append((mxpos, mx))
+                mn = this
+                mnpos = x[i]
+                lookformax = False
+        else:
+            if this > mn + delta:
+                mintab.append((mnpos, mn))
+                mx = this
+                mxpos = x[i]
+                lookformax = True
+
+    return np.array(maxtab), np.array(mintab)
