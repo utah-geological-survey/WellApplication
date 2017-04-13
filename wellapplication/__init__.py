6d36e3cfa426e5680ee0fe1506ef69e47d1804b2
Paul <paulinkenbrandt@utah.gov>
2017-04-13 08:57:23 -0600

    Update __init__.py

--- wellapplication/__init__.py
+++ wellapplication/__init__.py
@@ -9,7 +9,7 @@ from .mesopy import *
 from .graphs import *
 from .MannKendall import *
 
-__version__ = '0.4.25'
+__version__ = '0.4.26'
 __author__ = 'Paul Inkenbrandt'
 __name__ = 'wellapplication'
 
